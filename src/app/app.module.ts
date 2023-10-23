a2ea85337d2ba5be35a722a25d405c476fe112df
datecasp <ciudadano.fake@gmail.com>
2023-10-23 22:52:19 +0200

    added angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // Asegúrate de agregar esta importación
+import { MaterialModule } from './MaterialModule';
 
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import { WeatherService } from './weather.service'; // Debes mantener esta importación
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,10 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    FormsModule, // Agrega FormsModule al arreglo de imports
-    HttpClientModule
+    FormsModule,
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [WeatherService], // Agrega el servicio al arreglo de providers
   bootstrap: [AppComponent]
