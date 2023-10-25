96d7d10ef66177129c2273f79df034ffc50e6623
datecasp <ciudadano.fake@gmail.com>
2023-10-25 21:21:21 +0200

    style

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -5,12 +5,13 @@ import { List, Root } from '../models/five-days-forecast';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent {
   city = '';
   weatherData: List[] = [];
-  
+  icon: string ="";
+
   constructor(private weatherService: WeatherService) {}
 
   getFiveDayForecast() {
@@ -18,9 +19,58 @@ export class WeatherComponent {
       return;
     }
 
-    this.weatherService.getFiveDayForecast(this.city)
-      .subscribe(data => {
-        this.weatherData = data.list;
-      });
+    this.weatherService.getFiveDayForecast(this.city).subscribe((data) => {
+      this.icon = this.descriptionIconSelector(
+        data.list[0].weather[0].main,
+        data.list[0].weather[0].description
+      );
+      this.weatherData = data.list;
+    });
+  }
+
+  /**
+   * Selects icon to show based in wether main and description
+   * @param weatherMain 
+   * @param weatherDescription 
+   * @returns URI od icon to show
+   */
+  descriptionIconSelector(weatherMain: string, weatherDescription: string): string {
+    let icon: string = "";
+    switch (weatherMain) {
+      case 'Clouds':
+        if (
+          weatherDescription === 'few clouds' ||
+          weatherDescription === 'scattered clouds'
+        ) {
+          icon = 'assets/cloudy.png'; 
+        } else {
+          icon = 'assets/overcast.png'; 
+        }
+        break;
+      case 'Snow':
+        icon = 'assets/snow.png'; 
+        break;
+      case 'Clear':
+        icon = 'assets/sunny.png'; 
+        break;
+      case 'Drizzle':
+        icon = 'assets/lightrain.png'; 
+        break;
+      case 'Thunderstorm':
+        icon = 'assets/thunderstorm.png'; 
+        break;
+      case 'Rain':
+        if (weatherDescription === 'light rain') {
+          icon = 'assets/lightrain.png'; 
+        } else {
+          icon = 'assets/rain.png'; 
+        }
+        break;
+      default:
+        icon = 'assets/atmosphere.png'; 
+        break;
+    }
+
+    return icon;
   }
 }
